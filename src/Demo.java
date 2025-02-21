61bf60ee3316d8f36ae5a4fe08d5df8a27a7dfd7
ajaypandhavale <aj40135821@gmail.com>
2025-02-21 21:01:51 +0530

    restart in update

--- src/Demo.java
+++ src/Demo.java
@@ -2,11 +2,5 @@ public class Demo {
     public static void main(String[] args){
         System.out.println("Hii, Demo6");
 
-        System.out.println("adding after first commit");
-
-        System.out.println("pushing using update branch");
-
-
-        System.out.println("restart 1 using update");
     }
 }
